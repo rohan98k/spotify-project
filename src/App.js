63cef8e5c7598d082ee549e5556502b24bef761e
rohan98k <rohankamath1998@yahoo.com>
2020-09-28 03:08:32 +0530

    Basic Layout of Player

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
   useEffect(() => {
-    const token = getTokenFromUrl();
+    const hash = getTokenFromUrl();
     window.location.hash = '';
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+    }
+    //console.log(token);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  console.log('The user is >> ', user);
+  //console.log('The token is >> ', token);
 
   return (
     //BEM
     <div className="app">
-      <Login />
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,33 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  token:
+    'BQDDm7QhcqBT3QXdklefHKmGROaBjjN6s4WKPmOHw4JiiE9ZN8mpY4KM20SdcnhtJqdwsHuHbzlTay7CE7K3Xe_Erd6ZkQjHBJ_edFwXw3dQFXtdmSA5YITtsK89JGAmwgnY1f2OLBhTjo79T3UAKBTGkKkmD9Ge',
+  //Set token initial value to user token for debugging
+  //! Remove after Developing is finished
+};
+
+const reducer = (state, action) => {
+  //console.log(action);
+
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
