c996964a4fcf6c7e6bbd9d70b8c534592421ae40
rohan98k <rohankamath1998@yahoo.com>
2020-10-03 22:42:37 +0530

    SideBar Added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Sidebar.css';
+import SidebarOptions from './SidebarOptions';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1> I am the Sidebar</h1>
+      <img
+        className="sidebar__logo"
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Green.png"
+        alt="spotifylogo"
+      />
+      <SidebarOptions Icon={HomeIcon} title="Home" />
+      <SidebarOptions Icon={SearchIcon} title="Search" />
+      <SidebarOptions Icon={LibraryMusicIcon} title="Your Library" />
     </div>
   );
 }
